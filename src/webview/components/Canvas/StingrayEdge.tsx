b07e3541e883ed7848925cf8bd5df07de865a99a
Christopher Bohorquez <christopher.bohorquez@responsiveauto.com>
2025-06-20 16:28:49 -0400

    made the arrowhead smaller

--- src/webview/components/Canvas/StingrayEdge.tsx
+++ src/webview/components/Canvas/StingrayEdge.tsx
@@ -117,19 +117,19 @@ const StingrayEdge: React.FC<StingrayEdgeProps> = ({
   return (
     <>
       <defs>
-        <marker
-          id={`arrowhead-${id}`}
-          markerWidth="10"
-          markerHeight="10"
-          refX="9"
-          refY="5"
-          orient="auto"
-        >
-          <polygon
-            points="0 0, 10 5, 0 10"
-            fill={strokeColor}
-          />
-        </marker>
+      <marker
+        id={`arrowhead-${id}`}
+        markerWidth="6"
+        markerHeight="6"
+        refX="5"
+        refY="3"
+        orient="auto"
+      >
+        <polygon
+          points="0 0, 6 3, 0 6"
+          fill={strokeColor}
+        />
+      </marker>
       </defs>
       <path
         id={id}
